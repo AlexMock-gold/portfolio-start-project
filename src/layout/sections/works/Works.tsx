9d84c11c006dd8b3169a2a3005f8cd20280d531a
AlexMock <mokanua2020@gmail.com>
2024-08-05 23:24:08 +0300

    add:added structural components: works

---
+++ src/layout/sections/works/Works.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {SectionTitle} from "../../../componets/SectionTitle";
+
+export const Works = () => {
+    return (
+        <SectionTitle>My Works</SectionTitle>
+    );
+};
+
