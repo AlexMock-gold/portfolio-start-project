fbaf4a312619dd950f5302f88304df2e140a3102
AlexMock <mokanua2020@gmail.com>
2024-08-07 22:50:35 +0300

    developed the structure of the works section

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import {SectionTitle} from "../../../componets/SectionTitle";
+import styled from "styled-components";
+import {Menu} from "../../../componets/menu/Menu";
+import {FlexWrapper} from "../../../componets/FlexWrapper";
+import {Work} from "./work/Work";
+import socialImg from "./../../../assets/img/photo-2_1.webp";
+import timerImg from "./../../../assets/img/photo-3_1.webp";
+
+const worksItems = ["All", "Landing page", "React", "Spa"];
 
 export const Works = () => {
     return (
-        <SectionTitle>My Works</SectionTitle>
+        <StyledWorks>
+            <SectionTitle>My Works</SectionTitle>
+            <Menu menuItem={worksItems}/>
+            <FlexWrapper justify={"space-around"}>
+                <Work src={socialImg} title={"Social Network"} text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."}/>
+                <Work src={timerImg} title={"Timer"} text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit ut labore et dolore magna aliqua Ut enim"}/>
+            </FlexWrapper>
+    </StyledWorks>
     );
 };
+
+const StyledWorks = styled.section`
+    min-height: 100vh;
+    background-color: #d88e8e;
+`
 
