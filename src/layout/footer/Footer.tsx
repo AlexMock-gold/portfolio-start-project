5163f909694af017587b482da628ee8b12a09c92
AlexMock <mokanua2020@gmail.com>
2024-08-11 22:44:23 +0300

    creat: created section - footer

---
+++ src/layout/footer/Footer.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from "styled-components";
+import {SectionTitle} from "../../componets/SectionTitle";
+import {Icon} from "../../componets/icon/Icon";
+import {FlexWrapper} from "../../componets/FlexWrapper";
+
+export const Footer = () => {
+    return (
+        <StyledFooter>
+            <FlexWrapper align={"center"} direction={"column"}>
+            <Name>Svetlana</Name>
+            <SocialList>
+                <SocialItem><SocialLink href="#"><Icon height={"21px"} width={"21px"} viewBox={"0 0 21px 21px"}  iconId={"instagram"}/></SocialLink></SocialItem>
+                <SocialItem><SocialLink href="#"><Icon height={"21px"} width={"21px"} viewBox={"0 0 21px 21px"}  iconId={"telegram"}/></SocialLink></SocialItem>
+                <SocialItem><SocialLink href="#"><Icon height={"21px"} width={"21px"} viewBox={"0 0 21px 21px"}  iconId={"vKontakt"}/></SocialLink></SocialItem>
+                <SocialItem><SocialLink href="#"><Icon height={"21px"} width={"21px"} viewBox={"0 0 21px 21px"}  iconId={"linkeDin"}/></SocialLink></SocialItem>
+            </SocialList>
+            <Copyright>© 2023 Svetlana Dyablo, All Rights Reserved.</Copyright>
+            </FlexWrapper>
+        </StyledFooter>
+    );
+};
+
+
+const StyledFooter = styled.footer`
+    background-color: #eadd55;
+    min-height: 20vh;
+`
+
+const Name = styled.h3`
+
+`
+const SocialList = styled.ul`
+display: flex;
+    gap: 30px;
+    
+    
+`
+const SocialItem = styled.li`
+    list-style-type: none;
+`
+
+const SocialLink = styled.a`
+    
+`
+
+const Copyright = styled.small`
+
+`
