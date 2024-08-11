66358f01523353685914a437ee68d5dfef1c9645
AlexMock <mokanua2020@gmail.com>
2024-08-11 22:45:26 +0300

    creat: created section - footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from "styled-components";
-import {SectionTitle} from "../../componets/SectionTitle";
 import {Icon} from "../../componets/icon/Icon";
 import {FlexWrapper} from "../../componets/FlexWrapper";
 
