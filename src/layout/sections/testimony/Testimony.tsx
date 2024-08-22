e00a84b7b38bd619cba661f01d8db6ccf0893be0
AlexMock <mokanua2020@gmail.com>
2024-08-22 17:50:30 +0300

    styled section Testimony, and created Slider

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -4,15 +4,22 @@ import {SectionTitle} from "../../../componets/SectionTitle";
 import {Icon} from "../../../componets/icon/Icon";
 import {Slider} from "../../../componets/slider/Slider";
 import {FlexWrapper} from "../../../componets/FlexWrapper";
+import {IconWrapper} from "../skills/Skill";
+import {Container} from "../../../componets/Container";
 
 export const Testimony = () => {
     return (
         <StyledTestimony>
-            <SectionTitle>Testimony</SectionTitle>
-            <FlexWrapper align={"center"} direction={"column"}>
-                <Icon iconId={"quote"}/>
-                <Slider/>
-            </FlexWrapper>
+            <Container>
+                <SectionTitle>Testimony</SectionTitle>
+                <FlexWrapper align={"center"} direction={"column"}>
+                    <IconWrapper>
+                        <Icon iconId={"quote"}/>
+                    </IconWrapper>
+                    <Slider/>
+                </FlexWrapper>
+            </Container>
+
 
         </StyledTestimony>
     );
@@ -21,4 +28,8 @@ export const Testimony = () => {
 const StyledTestimony = styled.section`
     min-height: 50vh;
     background-color: #e8d45a;
+    
+    ${IconWrapper} {
+        margin: 52px 0 72px;
+    }
 `
