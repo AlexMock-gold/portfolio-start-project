44891a7fade7abcb2a5076b173f64a3b06d84499
AlexMock <mokanua2020@gmail.com>
2024-08-22 18:21:19 +0300

    styled section Slogan

--- src/layout/sections/slogan/Slogan.tsx
+++ src/layout/sections/slogan/Slogan.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import styled from "styled-components";
 import {SectionTitle} from "../../../componets/SectionTitle";
 import {Button} from "../../../componets/Button";
+import {Container} from "../../../componets/Container";
+import {FlexWrapper} from "../../../componets/FlexWrapper";
 
 export const Slogan = () => {
     return (
         <StyledSlogan>
-            <SectionTitle>I Am Available For Freelance</SectionTitle>
-            <Button>Hire me</Button>
+            <Container>
+                <FlexWrapper  align={"center"} direction={"column"}>
+                    <SectionTitle>I Am Available For Freelance</SectionTitle>
+                    <Button>Hire me</Button>
+                </FlexWrapper>
 
+            </Container>
         </StyledSlogan>
     );
 };
@@ -18,4 +24,5 @@ export default Slogan;
 const StyledSlogan = styled.section`
     min-height: 50vh;
     background-color: rgba(189, 175, 175, 0.58);
+    
 `
