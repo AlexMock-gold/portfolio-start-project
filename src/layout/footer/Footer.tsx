291c7ab28e52a9de17325c5846a299c6dfc8ef9d
AlexMock <mokanua2020@gmail.com>
2024-08-22 18:19:55 +0300

    styled footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from "styled-components";
 import {Icon} from "../../componets/icon/Icon";
 import {FlexWrapper} from "../../componets/FlexWrapper";
+import {theme} from "../../styles/Theme";
 
 export const Footer = () => {
     return (
@@ -22,27 +23,46 @@ export const Footer = () => {
 
 
 const StyledFooter = styled.footer`
-    background-color: #eadd55;
-    min-height: 20vh;
+    background-color: ${theme.colors.primaryBG};
+    padding: 40px 0;
 `
 
 const Name = styled.h3`
-
+    font-weight: 700;
+    font-size: 22px;
+    font-family: "Josefin Sans", sans-serif;
+    letter-spacing: 3px;
 `
 const SocialList = styled.ul`
 display: flex;
-    gap: 30px;
-    
+    gap: 20px;
+    margin: 30px 0;
     
 `
 const SocialItem = styled.li`
     list-style-type: none;
-`
-
-const SocialLink = styled.a`
     
 `
 
-const Copyright = styled.small`
+const SocialLink = styled.a`
+    background: rgba(255, 255, 255, 0.1);
+    border-radius: 50%;
+    height: 35px;
+    width: 35px;
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: ${theme.colors.accent};
+    &:hover {
+        color: ${theme.colors.primaryBG};
+        transform: translateY(-4px);
+    }
+`
 
+const Copyright = styled.small`
+    font-weight: 400; 
+    font-size: 12px;
+    text-align: center;
+    opacity: 0.5;
 `
