25631901d71b483ea4f3f4ea78c7002b39530fe6
AlexMock <mokanua2020@gmail.com>
2024-08-22 17:48:30 +0300

    I created a Theme file and put variables colors.

---
+++ src/styles/Theme.ts
@@ -0,0 +1,12 @@
+
+
+export const theme = {
+    colors: {
+        primaryBG: "#1F1F20",
+        secondaryBG: "#252527",
+        accent: "#7572D5",
+        font: "#FFFFFF",
+        borderColor: "#4a4a4a",
+        placeholderColor: "#495057",
+    }
+}
