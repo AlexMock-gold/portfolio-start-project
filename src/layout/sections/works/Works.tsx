4c4c8d8df0c84711561913d8056d0abfac46cf5d
AlexMock <mokanua2020@gmail.com>
2024-08-22 17:43:34 +0300

    styled the section Works

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import {SectionTitle} from "../../../componets/SectionTitle";
 import styled from "styled-components";
-import {Menu} from "../../../componets/menu/Menu";
+import {TabMenu} from "./tabMenu/TabMenu";
 import {FlexWrapper} from "../../../componets/FlexWrapper";
 import {Work} from "./work/Work";
 import socialImg from "./../../../assets/img/photo-2_1.webp";
 import timerImg from "./../../../assets/img/photo-3_1.webp";
+import {Container} from "../../../componets/Container";
 
 const worksItems = ["All", "Landing page", "React", "Spa"];
 
 export const Works = () => {
     return (
         <StyledWorks>
-            <SectionTitle>My Works</SectionTitle>
-            <Menu menuItem={worksItems}/>
-            <FlexWrapper justify={"space-around"}>
-                <Work src={socialImg} title={"Social Network"} text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."}/>
-                <Work src={timerImg} title={"Timer"} text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit ut labore et dolore magna aliqua Ut enim"}/>
-            </FlexWrapper>
+            <Container>
+                <SectionTitle>My Works</SectionTitle>
+                <TabMenu menuItem={worksItems}/>
+                <FlexWrapper justify={"space-between"} align={"flex-start"}>
+                    <Work src={socialImg} title={"Social Network"} text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."}/>
+                    <Work src={timerImg} title={"Timer"} text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit ut labore et dolore magna aliqua Ut enim"}/>
+
+                </FlexWrapper>
+            </Container>
     </StyledWorks>
     );
 };
 
 const StyledWorks = styled.section`
-    min-height: 100vh;
-    background-color: #d88e8e;
+
 `
 
--- src/layout/sections/works/work/Work.tsx
+++ src/layout/sections/works/work/Work.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled from "styled-components";
+import {Link} from "../../../../componets/Link";
+import {theme} from "../../../../styles/Theme";
+import {Button} from "../../../../componets/Button";
 
 type WorkPropsType = {
     title: string
@@ -10,32 +13,81 @@ type WorkPropsType = {
 export const Work = (props: WorkPropsType) => {
     return (
         <StyledWork>
-            <Image src={props.src} alt=""/>
-            <Title>{props.title}</Title>
-            <Text>{props.text}</Text>
-            <Link href={"#"}>demo</Link>
-            <Link href={"#"}>code</Link>
+            <ImgWrapper>
+                <Image src={props.src} alt=""/>
+                <Button>view project</Button>
+            </ImgWrapper>
+            <Description>
+                <Title>{props.title}</Title>
+                <Text>{props.text}</Text>
+                <Link href={"#"}>demo</Link>
+                <Link href={"#"}>code</Link>
+            </Description>
+
         </StyledWork>
     );
 };
 
 const StyledWork = styled.div`
-    background-color: #e8d45a;
+    background-color: ${theme.colors.secondaryBG};
     max-width: 540px;
     width: 100%;
+    
+    ${Link} {
+        padding: 10px 0;
+        & + ${Link}{
+            margin-left: 20px;
+        }
+    }
 `
+
+const ImgWrapper = styled.div`
+    position: relative;
+    
+    
+&:hover {
+    &::before {
+        content: "";
+        position: absolute;
+        right: 0;
+        left: 0;
+        top: 0 ;
+        bottom: 0;
+        backdrop-filter: blur(4px);
+        background: rgba(0, 0, 0, 0.3);
+    }
+        ${Button} {
+            opacity: 1;
+        }
+
+}
+    ${Button} {
+        opacity: 0;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+
+        &::before {
+            height: 100%;
+            width: 100%;
+        }
+    }
+`
+
 const Image = styled.img`
 width: 100%;
     height: 260px;
     object-fit: cover;
 `
+const Description = styled.div`
+    margin: 25px 20px;
+`
 
 const Title = styled.h3`
-
+        margin-top: 25px;
 `
 const Text = styled.p`
-
+    margin: 14px 0 10px 0;
 `
-const Link = styled.a`
 
-`
